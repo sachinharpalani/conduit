9d405d639a2981fe588cbe66079747ac4d139b2a
Sachin Harpalani <sachinharpalani3795@gmail.com>
2018-05-31 16:38:18 +0530

    Completed GET /users api

--- apps/authentication/jwt.py
+++ apps/authentication/jwt.py
@@ -4,3 +4,7 @@ from django.conf.global_settings import SECRET_KEY
 def get_token(payload):
     token = jwt.encode(payload,SECRET_KEY,algorithm='HS256')
     return str(token,'utf-8')
+
+def get_payload(token):
+    payload = jwt.decode(token,SECRET_KEY,algorihms=['HS256'])
+    return payload
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from .serializers import AuthorSerializer
 from .models import Author
 from django.contrib.auth.models import User
-from .jwt import get_token
+from .jwt import get_token,get_payload
 import json
 
 # Create your views here.
@@ -13,6 +13,7 @@ import json
 @csrf_exempt
 @api_view(['GET','POST','PUT'])
 def user_operations(request):
+
     if request.method == 'POST':
         user_obj = json.loads(request.POST['user'])
         username = user_obj.get('username')
@@ -27,6 +28,27 @@ def user_operations(request):
             author = Author(user=user)
             author.token = str(get_token({'user_id' : user.id}))
             author.save()
+        except:
+            error_response = JsonResponse({"errors" : {"body" : ["Unexpected Error"]}})
+            error_response.status_code=422
+            return error_response
+
+        success_response = JsonResponse({"user" : {"email" : user.email,"token" : user.author.token, "username" : user.username , "bio" : user.author.bio , "image" : user.author.image}})
+        success_response.status_code=201
+        return success_response
+
+    elif request.method == 'GET':
+        try:
+            token = request.META['HTTP_AUTHORIZATION']
+        except:
+            error_response = JsonResponse({"errors" : {"body" : ["Unauthorized"]}})
+            error_response.status_code=401
+            return error_response
+
+        try:
+            payload = get_payload(token)
+            user = User.objects.get(pk=payload["user_id"])
+
         except:
             error_response = JsonResponse({"errors" : {"body" : ["Unexpected Error"]}})
             error_response.status_code=422
